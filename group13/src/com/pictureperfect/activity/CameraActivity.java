6c65c9ad1c4530a3c18a44eda565061bb0dbac8c
p.adityarao@gmail.com <p.adityarao@gmail.com@b3f1a258-f284-1c90-0ac6-ef47271b87f8>
2012-11-02 23:21:00 +0000

    Added the rotation

--- group13/src/com/pictureperfect/activity/CameraActivity.java
+++ group13/src/com/pictureperfect/activity/CameraActivity.java
@@ -1,16 +1,13 @@
 package com.pictureperfect.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -49,114 +46,131 @@ public class CameraActivity extends Activity {
 	 * } public static int getCameraPicRequest() { return CAMERA_PIC_REQUEST; }
 	 */
 
-	
-		private SurfaceView preview = null;
-		private SurfaceHolder previewHolder = null;
-		private Camera camera = null;
-		private boolean inPreview = false;
-		private boolean cameraConfigured = false;
+	private SurfaceView preview = null;
+	private SurfaceHolder previewHolder = null;
+	private Camera camera = null;
+	private boolean inPreview = false;
+	private boolean cameraConfigured = false;
 
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-			setContentView(R.layout.camera);
+		setContentView(R.layout.camera);
 
-			preview = (SurfaceView) findViewById(R.id.preview);
-			previewHolder = preview.getHolder();
-			previewHolder.addCallback(surfaceCallback);
-			previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		preview = (SurfaceView) findViewById(R.id.preview);
+		previewHolder = preview.getHolder();
+		previewHolder.addCallback(surfaceCallback);
+		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		camera = Camera.open();
+		startPreview();
+	}
+
+	@Override
+	public void onPause() {
+		if (inPreview) {
+			camera.stopPreview();
 		}
 
-		@Override
-		public void onResume() {
-			super.onResume();
+		camera.release();
+		camera = null;
+		inPreview = false;
 
-			camera = Camera.open();
+		super.onPause();
+	}
+
+	private Camera.Size getBestPreviewSize(int width, int height,
+			Camera.Parameters parameters) {
+		
+		int rotation = this.getWindowManager().getDefaultDisplay()
+				.getRotation();
+		int degrees = 0;
+		switch (rotation) {
+		case Surface.ROTATION_0:
+			degrees = 0;
+			break;
+		case Surface.ROTATION_90:
+			degrees = 90;
+			break;
+		case Surface.ROTATION_180:
+			degrees = 180;
+			break;
+		case Surface.ROTATION_270:
+			degrees = 270;
+			break;
+		}
+
+		camera.setDisplayOrientation((360 - degrees + 90) % 360);
+		Camera.Size result = null;
+		for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+			if (size.width <= width && size.height <= height) {
+				if (result == null) {
+					result = size;
+				} else {
+					int resultArea = result.width * result.height;
+					int newArea = size.width * size.height;
+
+					if (newArea > resultArea) {
+						result = size;
+					}
+				}
+			}
+		}
+
+		return (result);
+	}
+
+	private void initPreview(int width, int height) {
+		if (camera != null && previewHolder.getSurface() != null) {
+			try {
+				camera.setPreviewDisplay(previewHolder);
+			} catch (Throwable t) {
+				Log.e("PreviewDemo-surfaceCallback",
+						"Exception in setPreviewDisplay()", t);
+				Toast.makeText(CameraActivity.this, t.getMessage(),
+						Toast.LENGTH_LONG).show();
+			}
+
+			if (!cameraConfigured) {
+				Camera.Parameters parameters = camera.getParameters();
+				Camera.Size size = getBestPreviewSize(width, height, parameters);
+
+				if (size != null) {
+					parameters.setPreviewSize(size.width, size.height);
+					camera.setParameters(parameters);
+					cameraConfigured = true;
+				}
+			}
+		}
+	}
+
+	private void startPreview() {
+		if (cameraConfigured && camera != null) {
+			camera.startPreview();
+			inPreview = true;
+		}
+	}
+
+	SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+		public void surfaceCreated(SurfaceHolder holder) {
+			// no-op -- wait until surfaceChanged()
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			initPreview(width, height);
 			startPreview();
 		}
 
-		@Override
-		public void onPause() {
-			if (inPreview) {
-				camera.stopPreview();
-			}
-
-			camera.release();
-			camera = null;
-			inPreview = false;
-
-			super.onPause();
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// no-op
 		}
+	};
 
-		private Camera.Size getBestPreviewSize(int width, int height,
-				Camera.Parameters parameters) {
-			Camera.Size result = null;
-
-			for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-				if (size.width <= width && size.height <= height) {
-					if (result == null) {
-						result = size;
-					} else {
-						int resultArea = result.width * result.height;
-						int newArea = size.width * size.height;
-
-						if (newArea > resultArea) {
-							result = size;
-						}
-					}
-				}
-			}
-
-			return (result);
-		}
-
-		private void initPreview(int width, int height) {
-			if (camera != null && previewHolder.getSurface() != null) {
-				try {
-					camera.setPreviewDisplay(previewHolder);
-				} catch (Throwable t) {
-					Log.e("PreviewDemo-surfaceCallback",
-							"Exception in setPreviewDisplay()", t);
-					Toast.makeText(CameraActivity.this, t.getMessage(),
-							Toast.LENGTH_LONG).show();
-				}
-
-				if (!cameraConfigured) {
-					Camera.Parameters parameters = camera.getParameters();
-					Camera.Size size = getBestPreviewSize(width, height,
-							parameters);
-
-					if (size != null) {
-						parameters.setPreviewSize(size.width, size.height);
-						camera.setParameters(parameters);
-						cameraConfigured = true;
-					}
-				}
-			}
-		}
-
-		private void startPreview() {
-			if (cameraConfigured && camera != null) {
-				camera.startPreview();
-				inPreview = true;
-			}
-		}
-
-		SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-			public void surfaceCreated(SurfaceHolder holder) {
-				// no-op -- wait until surfaceChanged()
-			}
-
-			public void surfaceChanged(SurfaceHolder holder, int format,
-					int width, int height) {
-				initPreview(width, height);
-				startPreview();
-			}
-
-			public void surfaceDestroyed(SurfaceHolder holder) {
-				// no-op
-			}
-		};
-
-	}
+}
