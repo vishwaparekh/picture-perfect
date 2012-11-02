708daa034a0663c8e4493b917bdffb53506117cc
p.adityarao@gmail.com <p.adityarao@gmail.com@b3f1a258-f284-1c90-0ac6-ef47271b87f8>
2012-11-02 00:24:54 +0000

    Camera preview generated

--- group13/src/com/pictureperfect/activity/CameraActivity.java
+++ group13/src/com/pictureperfect/activity/CameraActivity.java
@@ -2,51 +2,161 @@ package com.pictureperfect.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * This screen helps the user to click a burst of images.
+ * 
  * @author group13
- *
+ * 
  */
 public class CameraActivity extends Activity {
-	
+
 	private static final int CAMERA_PIC_REQUEST = 1337;
 	private PictureCallback rawCallback;
 
-    /** Called when the activity is first created. */
-    @Override
-    
-    /**Called when the activity is first created. This is where you should do all 
-     * of your normal static set up: create views, bind data to lists, etc. This method
-     *  also provides you with a Bundle containing the activity's previously frozen 
-     *  state, if there was one.
-     * 
-     * @param savedInstancesState f the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle) 
-     */
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.camera);
-        Button CamButton = (Button) findViewById(R.id.cameraButton);
-        
-        CamButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(CameraActivity.this,
-						SelectBackground.class);
-				startActivity(intent);
+	/** Called when the activity is first created. */
+
+	/**
+	 * Called when the activity is first created. This is where you should do
+	 * all of your normal static set up: create views, bind data to lists, etc.
+	 * This method also provides you with a Bundle containing the activity's
+	 * previously frozen state, if there was one.
+	 * 
+	 * @param savedInstancesState
+	 *            f the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle)
+	 */
+	/*
+	 * public void onCreate(Bundle savedInstanceState) {
+	 * super.onCreate(savedInstanceState); setContentView(R.layout.camera);
+	 * Button CamButton = (Button) findViewById(R.id.cameraButton);
+	 * 
+	 * CamButton.setOnClickListener(new OnClickListener() { public void
+	 * onClick(View v) { Intent intent = new Intent(CameraActivity.this,
+	 * SelectBackground.class); startActivity(intent); } });
+	 * 
+	 * } public static int getCameraPicRequest() { return CAMERA_PIC_REQUEST; }
+	 */
+
+	
+		private SurfaceView preview = null;
+		private SurfaceHolder previewHolder = null;
+		private Camera camera = null;
+		private boolean inPreview = false;
+		private boolean cameraConfigured = false;
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+
+			setContentView(R.layout.camera);
+
+			preview = (SurfaceView) findViewById(R.id.preview);
+			previewHolder = preview.getHolder();
+			previewHolder.addCallback(surfaceCallback);
+			previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+
+			camera = Camera.open();
+			startPreview();
+		}
+
+		@Override
+		public void onPause() {
+			if (inPreview) {
+				camera.stopPreview();
 			}
-		});
-        
-    }
-	public static int getCameraPicRequest() {
-		return CAMERA_PIC_REQUEST;
+
+			camera.release();
+			camera = null;
+			inPreview = false;
+
+			super.onPause();
+		}
+
+		private Camera.Size getBestPreviewSize(int width, int height,
+				Camera.Parameters parameters) {
+			Camera.Size result = null;
+
+			for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+				if (size.width <= width && size.height <= height) {
+					if (result == null) {
+						result = size;
+					} else {
+						int resultArea = result.width * result.height;
+						int newArea = size.width * size.height;
+
+						if (newArea > resultArea) {
+							result = size;
+						}
+					}
+				}
+			}
+
+			return (result);
+		}
+
+		private void initPreview(int width, int height) {
+			if (camera != null && previewHolder.getSurface() != null) {
+				try {
+					camera.setPreviewDisplay(previewHolder);
+				} catch (Throwable t) {
+					Log.e("PreviewDemo-surfaceCallback",
+							"Exception in setPreviewDisplay()", t);
+					Toast.makeText(CameraActivity.this, t.getMessage(),
+							Toast.LENGTH_LONG).show();
+				}
+
+				if (!cameraConfigured) {
+					Camera.Parameters parameters = camera.getParameters();
+					Camera.Size size = getBestPreviewSize(width, height,
+							parameters);
+
+					if (size != null) {
+						parameters.setPreviewSize(size.width, size.height);
+						camera.setParameters(parameters);
+						cameraConfigured = true;
+					}
+				}
+			}
+		}
+
+		private void startPreview() {
+			if (cameraConfigured && camera != null) {
+				camera.startPreview();
+				inPreview = true;
+			}
+		}
+
+		SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+			public void surfaceCreated(SurfaceHolder holder) {
+				// no-op -- wait until surfaceChanged()
+			}
+
+			public void surfaceChanged(SurfaceHolder holder, int format,
+					int width, int height) {
+				initPreview(width, height);
+				startPreview();
+			}
+
+			public void surfaceDestroyed(SurfaceHolder holder) {
+				// no-op
+			}
+		};
+
 	}
-    
-    
-}
