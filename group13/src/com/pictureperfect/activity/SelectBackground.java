f3bf285b1970430ca822baeb6bf5f0db6fc4c93f
p.adityarao@gmail.com <p.adityarao@gmail.com@b3f1a258-f284-1c90-0ac6-ef47271b87f8>
2012-11-15 05:58:21 +0000

    Added File browsing classes for Edit Existing

--- group13/src/com/pictureperfect/activity/SelectBackground.java
+++ group13/src/com/pictureperfect/activity/SelectBackground.java
@@ -1,15 +1,23 @@
 package com.pictureperfect.activity;
 
-import java.util.Vector;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * This screen helps the user to choose the background image.
@@ -18,7 +26,14 @@ import android.widget.Button;
  */
 public class SelectBackground extends Activity {
 
+	ImgData imgData;
+
+	private SurfaceView m_previewSurfaceView = null;
+	private SurfaceHolder m_previewSurfaceHolder = null;
+	
 	private Canvas myCanvas;
+	
+	private Paint myPaint= new Paint(Paint.ANTI_ALIAS_FLAG);
 
 	private Bitmap[] myPictures;
 
@@ -38,8 +53,16 @@ public class SelectBackground extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.selectbackground);
-
-		Button doneButton = (Button) findViewById(R.id.backgroundDone); // INSERT
+		
+		m_previewSurfaceView = (SurfaceView) findViewById(R.id.preview);
+		m_previewSurfaceHolder = m_previewSurfaceView.getHolder();
+		m_previewSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		m_previewSurfaceHolder.addCallback(cameraSurfaceCallback);
+		
+		Intent i = getIntent();
+		imgData = (ImgData)i.getSerializableExtra("imgData");
+		init();
+		/*Button doneButton = (Button) findViewById(R.id.backgroundDone); // INSERT
 																		// BUTTON
 		doneButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -47,13 +70,44 @@ public class SelectBackground extends Activity {
 						ChooseOptionActivity.class);
 				startActivity(intent);
 			}
-		});
+		});*/
 	}
+
+	
+	private void init() {
+		imgData.setBackgroundNum(0);
+		try {
+			myCanvas= new Canvas(imgData.getBackground());	
+		} catch (NullPointerException e) {
+			
+		}
+		
+		myPaint.setStyle(Paint.Style.STROKE);
+		myPaint.setStrokeCap(Paint.Cap.ROUND);
+		myPaint.setColor(0x80ff0000);
+		myPaint.setStrokeWidth(3);
+	}
+
+
+	SurfaceHolder.Callback cameraSurfaceCallback = new SurfaceHolder.Callback() {
+		public void surfaceCreated(SurfaceHolder holder) {
+			
+		}
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+	
+		}
+
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// remove the holder
+		}
+	};
 
 	/**
 	 * This method updates the background number in both the view and the ImgData object.
 	 */
 	private void changeBackground() {
+		
 	}
 
 }
